b7dd48b3383d1805be77d3b455ff7394f685a4fe
Adnan Izhar <adnan.izhar@cooperativecomputing.com>
2023-01-30 20:28:43 +0500

    feature/ECWA 100/Dashboard/ 2.5 hours

--- components/dashboard/header.vue
+++ components/dashboard/header.vue
@@ -1,11 +1,15 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div class="header">
-        Header
-    </div>
+  <div class="bg-[#F5F5F5] h-[90px] w-full flex justify-end items-center">
+    <img src="~/assets/images/Component.png" class="mr-8" />
+    <img src="~/assets/images/Group.png" class="mr-8" />
+    <img
+      src="~/assets/images/Ellipse.png"
+      class="h-10 border-4 border-indigo-600 mr-2"
+    />
+    <p class="mr-10">Admin</p>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- components/dashboard/sidebar.vue
+++ components/dashboard/sidebar.vue
@@ -1,43 +1,88 @@
 <template>
-    <div>
-        <button @click="SidebarState.sidebarOpen = !SidebarState.sidebarOpen">Toggle Sidebar</button>
-        <div :class="{ 'sidebar': true, 'open': SidebarState.sidebarOpen }">
-            <nav>
-                <ul>
-                    <li v-for="item in items" :key="item.link">
-                        <NuxtLink :to="item.link">{{ item.text }}</NuxtLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+  <div>
+    <div class="sidebar-toggle-btn">
+      <button @click="SidebarState.sidebarOpen = !SidebarState.sidebarOpen">
+        Toggle Sidebar
+      </button>
     </div>
+    <div
+      class="shadow-2xl"
+      :class="{ sidebar: true, open: SidebarState.sidebarOpen }"
+    >
+      <div class="flex justify-center">
+        <img
+          class="h-20 mt-4"
+          alt="logo"
+          :src="
+            '_nuxt/assets/images/' +
+            (SidebarState.sidebarOpen
+              ? 'SmartSuiteLogo.png'
+              : 'SmartSuite-Final-Logo.png')
+          "
+        />
+      </div>
+      <div class="flex flex-col">
+        <div class="mt-8">
+          <nav>
+            <ul>
+              <li v-for="item in items" :key="item.link">
+                <NuxtLink class="text-[#a3a3a3] no-underline" :to="item.link"
+                  ><img :src="'_nuxt/assets/images/' + item.icon" />
+                  {{ SidebarState.sidebarOpen ? item.text : "" }}</NuxtLink
+                >
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <ul>
+            <li>Logout</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-
 const items = [
-    { text: 'Home', link: '/' },
-    { text: 'About', link: '/' },
-    { text: 'Contact', link: '/' },
+  { text: "Dashboard", link: "/", icon: "Dashboard.png" },
+  { text: "Manage Users", link: "/", icon: "people.png" },
+  { text: "Email / Messages", link: "/", icon: "email.png" },
+  { text: "Recipients and Groups", link: "/", icon: "Recipients-Groups.png" },
 ];
 
 const SidebarState = reactive({
-    sidebarOpen: false
-})
-
+  sidebarOpen: true,
+});
 </script>
 
 <style lang="scss">
+.sidebar-toggle-btn {
+  position: absolute;
+  left: 300px;
+  top: 28px;
+}
 .sidebar {
-    position: absolute;
-    width: 90px;
-    height: 100vh;
-    background: red;
-    z-index: 1;
-    transition: 0.3s;
-
-    &.open {
-        width: 317px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 90px;
+  height: 100vh;
+  background: #ffffff;
+  z-index: 1;
+  transition: 0.3s;
+  ul {
+    li {
+      color: #a3a3a3 !important;
+      padding: 20px 2px 20px 40px;
+      border-radius: 6px;
+      cursor: pointer;
     }
+  }
+
+  &.open {
+    width: 317px;
+  }
 }
 </style>
