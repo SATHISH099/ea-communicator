70a390b5ef314f2b930ae9ee69862a5cb9ebaede
Faraz Ahmed Khan <faraz.ahmed@cooperativecomputing.com>
2023-02-20 20:03:24 +0500

    FE/ECWA-159 #comment Worked on Library #time 2h

---
+++ pages/email-messages/library/index.vue
@@ -0,0 +1,109 @@
+<script lang="ts" setup>
+import Multiselect from '@vueform/multiselect/src/Multiselect';
+</script>
+
+<template>
+  <div>
+    <div mb-8>
+      <div mb-10>
+        <h4 class="mb-4 text-stone">Library</h4>
+        <p class="text-silver">
+          Communicator / Email/Messages /
+          <span class="text-primary">Library</span>
+        </p>
+      </div>
+      <div class="flex flex-wrap justify-between items-center gap-4">
+        <div class="flex flex-wrap items-center gap-4">
+          <FormKit
+            prefix-icon="search"
+            type="search"
+            placeholder="Search media items"
+            input-class="form-control pl-[3.5rem]"
+            prefix-icon-class="search-icon"
+            outer-class="md:w-[34rem] w-full"
+          />
+          <button class="btn btn-primary md:w-30 w-full">Search</button>
+        </div>
+        <div flex items-center gap-4>
+          <button
+            class="border-none outline-none flex items-center py-[16px] px-[32px] rounded-[4px] text-stone text-[1.125rem] gap-3 cursor-pointer"
+          >
+            <img src="/document-add.png" alt="" /> Add new item
+          </button>
+          <Multiselect
+            placeholder="Category"
+            :options="['test1', 'test2']"
+            class="md:w-[14rem] w-full"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="bg-white small-shadow p-6">
+      <div class="grid lg:grid-cols-7 md:grid-cols-3 grid-cols-1 gap-3">
+        <div class="max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+        <div class="library-image max-w-[12rem] cursor-pointer relative">
+          <img w-full object-cover object-center src="/image.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
