3f8e536852677e0322d48b60c985094e79b61357
razabangi <muhammadrazabangi9@gmail.com>
2023-03-24 22:20:37 +0500

    ECWA-447 #time 10m #comment Dashboard || Calendar || The ranges should be till the date today not future issue has been resolved

--- components/DatePicker.vue
+++ components/DatePicker.vue
@@ -3,7 +3,7 @@ import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
 const emit = defineEmits(['setDate']);
 
-const date = ref(new Date());
+const date = ref([new Date(), new Date()]);
 const handleDate = (dateStr: string) => {
   emit('setDate', dateStr);
 };
@@ -13,10 +13,14 @@ const handleDate = (dateStr: string) => {
   <div class="datepicker">
     <VueDatePicker
       v-model="date"
+      type="date"
+      :time="false"
+      format="MM/dd/yyyy"
+      :max-date="new Date()"
       range
       multi-calendars
-      @update:model-value="handleDate"
       placeholder="Filter By: Date"
+      @update:model-value="handleDate"
     />
   </div>
 </template>
