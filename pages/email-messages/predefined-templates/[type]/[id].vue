bc24cc371fb9a51da1b8da3122f3eff82095b379
razabangi <muhammadrazabangi9@gmail.com>
2023-04-21 00:31:05 +0500

    resolve eslint issues

--- pages/email-messages/predefined-templates/[type]/[id].vue
+++ pages/email-messages/predefined-templates/[type]/[id].vue
@@ -29,34 +29,41 @@ if (type.value === 'email') {
   <div>
     <div class="flex flex-wrap justify-between items-center mb-0 md:mb-10">
       <div class="md:mb-0 mb-10">
-          <h4 class="mb-4 text-stone">Predefined Templates</h4>
-          <p class="text-silver">
-            <NuxtLink to="/" class="text-silver sub-heading"
-              >Communicator</NuxtLink
-            >
-            <span class="text-silver">/</span>
-            <NuxtLink to="/email-messages" class="text-silver sub-heading">
-              Email / Alerts</NuxtLink
-            >
-            <span class="text-silver">/</span>
-            <NuxtLink to="/email-messages/predefined-templates" class="text-silver sub-heading">
-              Predefined Templates</NuxtLink
-            >
-            <span class="text-silver">/</span>
-            <span class="text-primary"> View Predefined Template</span>
-          </p>
-        </div>
+        <h4 class="mb-4 text-stone">Predefined Templates</h4>
+        <p class="text-silver">
+          <NuxtLink to="/" class="text-silver sub-heading"
+            >Communicator</NuxtLink
+          >
+          <span class="text-silver">/</span>
+          <NuxtLink to="/email-messages" class="text-silver sub-heading">
+            Email / Alerts</NuxtLink
+          >
+          <span class="text-silver">/</span>
+          <NuxtLink
+            to="/email-messages/predefined-templates"
+            class="text-silver sub-heading"
+          >
+            Predefined Templates</NuxtLink
+          >
+          <span class="text-silver">/</span>
+          <span class="text-primary"> View Predefined Template</span>
+        </p>
+      </div>
     </div>
     <DashboardCard title="Add New Predefined Template">
       <div class="p-6">
         <div class="max-w-[50rem]">
           <div class="mb-10 grid gap-y-2">
-            <h6 class="text-stone">{{ type === 'email' ? 'Subject' : 'Title' }}</h6>
+            <h6 class="text-stone">
+              {{ type === 'email' ? 'Subject' : 'Title' }}
+            </h6>
             <p class="text-carbon">{{ title }}</p>
           </div>
 
           <div class="mb-10 grid gap-y-2">
-            <h6 class="text-stone">{{ type === 'email' ? 'Body' : 'Message' }}</h6>
+            <h6 class="text-stone">
+              {{ type === 'email' ? 'Body' : 'Message' }}
+            </h6>
             <div v-html="body"></div>
           </div>
         </div>
