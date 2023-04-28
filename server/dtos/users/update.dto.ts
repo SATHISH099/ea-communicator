bdb7f1d359aae7a4034765860cc278e084decd02
Hasan Raza <hasan.raza@cooperativecomputing.com>
2023-04-29 04:57:10 +0500

    fix: user update endpoint

--- server/dtos/users/update.dto.ts
+++ server/dtos/users/update.dto.ts
@@ -1,3 +1,34 @@
-import { CreateUserDto } from './create.dto';
+import { IsArray, IsBoolean, IsOptional, IsString } from 'class-validator';
 
-export class UpdateUserDto extends CreateUserDto {}
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  email: string;
+
+  @IsBoolean()
+  @IsOptional()
+  status: boolean;
+
+  @IsString()
+  @IsOptional()
+  department: string;
+
+  @IsString()
+  @IsOptional()
+  team: string;
+
+  @IsString()
+  @IsOptional()
+  notes: string;
+
+  @IsOptional()
+  role: number;
+
+  @IsOptional()
+  @IsArray()
+  roles: number[];
+}
